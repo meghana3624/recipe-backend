4c7cea89155bc90f701e8e9309c3f443125d40cf
Meghana Kambhampati <meghana.kambhampati2021@vitstudent.ac.in>
2023-11-23 00:43:44 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+import {userRouter} from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app=express();
+
+app.use(express.json());
+app.use(cors()); 
+
+app.use("/auth",userRouter);
+app.use("/recipes",recipesRouter);
+
+mongoose.connect("mongodb+srv://recipes:MernStack123@recipes.mr8bpob.mongodb.net/recipes?retryWrites=true&w=majority")
+//app.listen(3001,()=>console.log("SERVER STARTED!"));
+
+const PORT = process.env.PORT || 3001; // set server port
+// Use environment variable for port
+//const port = process.env.PORT || 3000; // default to 3000 if PORT is not set
+app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}!`));
